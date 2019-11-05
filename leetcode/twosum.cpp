5de7ad717e9887e548c1e67726f319c64b8119e4
rwinston <rory.winston@gmail.com>
2019-11-05 14:20:04 +0800

    twosum

---
+++ leetcode/twosum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> solution;
+        for (int i=0;i<nums.size();++i) {
+            for (int j=i;j<nums.size();++j) {
+                if (nums[i]+nums[j]==target) {
+                    solution.push_back(i);
+                    solution.push_back(j);
+                }
+            }
+        }
+        return solution;
+    }
+};
