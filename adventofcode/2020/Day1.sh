e19b627bce0bae67af0cf2572610548ca4967808
rwinston <rory@theresearchkitchen.com>
2020-12-02 19:10:51 +0800

    Add

---
+++ adventofcode/2020/Day1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# AWK
+
+# Data in a file called p1
+# Part 1
+cat p1 | awk 'BEGIN {} {A[$1]=2020-$1} END { for (i in A) {if (A[i] in A) print(A[i]*i)} }'
+
+# Part 2
+cat p1 | awk 'BEGIN {} {A[$1]=$1} END {for (i in A) {for (j in A){if (i!=j) {k=2020-(i+j); if(k in A){ print(i*j*k); break}}}}}'
