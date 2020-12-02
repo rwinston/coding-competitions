89718af8c62644f4cbe8d0d49c94b3145a5edd9c
Rory Winston <rory.winston@gmail.com>
2020-12-02 18:06:03 +0800

    Update Day2_Part1.pl

--- 2020/Day2_Part1.pl
+++ 2020/Day2_Part1.pl
@@ -8,6 +8,5 @@ foreach my $line (<STDIN>) {
         chomp($line);
         my @parts = ($line =~ /^(\d+)\-(\d+) ([a-z]): (\S+)$/g);
         my $count = () = ($parts[3] =~ /$parts[2]/g);
-        print "$parts[0] $parts[1] $parts[2] $parts[3] $count\n";
         if ($count >= $parts[0] && $count <= $parts[1]) { $valid++; }
 }
