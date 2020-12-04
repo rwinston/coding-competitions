960e6ae6b54f5cc7692e55515f668aa67716ffd9
Rory Winston <rory.winston@gmail.com>
2020-12-04 17:50:12 +0800

    Update Day4.js

--- adventofcode/2020/Day4.js
+++ adventofcode/2020/Day4.js
@@ -19,18 +19,18 @@ for (let entry of lines) {
 
 let requiredFields = ["byr","iyr","eyr","hgt","hcl","ecl","pid"];
 let validation = {
-    "byr" : function(x) { return x>=1920 && x <=2002},
-    "iyr" : function(x) { return x>=2010 && x <=2020},
-    "eyr" : function(x) { return x>=2020 && x <=2030},
+    "byr" : function(x) { return parseInt(x)>=1920 && parseInt(x) <=2002},
+    "iyr" : function(x) { return parseInt(x)>=2010 && parseInt(x) <=2020},
+    "eyr" : function(x) { return parseInt(x)>=2020 && parseInt(x) <=2030},
     "hgt" : function(x) { 
         const re = /([0-9]+)(in|cm)/; 
         let m = x.match(re);
         if (m == null) return false;
-        return (m[2] == "in" ? m[1] >= 59 && m[1] <= 76 : m[1] >= 150 && m[1] <= 193)
+        return (m[2] == "in" ? parseInt(m[1]) >= 59 && parseInt(m[1]) <= 76 : parseInt(m[1]) >= 150 && parseInt(m[1]) <= 193)
         },
-    "hcl" : function(x) { return /#[0-9a-f]{6}/.test(x); },
-    "ecl" : function(x) { return /(amb|blu|gry|brn|grn|hzl|oth)/.test(x); },
-    "pid" : function(x) { return /([0-9]{9})/.test(x); },
+    "hcl" : function(x) { return /^#[0-9a-f]{6}$/.test(x); },
+    "ecl" : function(x) { return /^(amb|blu|gry|brn|grn|hzl|oth)$/.test(x); },
+    "pid" : function(x) { return /^([0-9]{9})$/.test(x); },
     "cid" : function(x) { return true; }
 };
 
@@ -38,7 +38,7 @@ let validation = {
 let validPassports = 0;
 for (let passport of passports) {
     // Part1 
-    //if (requiredFields.every(prop => passport.hasOwnProperty(prop))) { validPassports++; }
+    //if (requiredFields.every(prop => passport.hasOwnProperty(prop) && validation[prop](passport[prop]))) { validPassports++; }
     // Part2
     if (requiredFields.every(prop => passport.hasOwnProperty(prop) && validation[prop](passport[prop]))) { validPassports++; }
 
